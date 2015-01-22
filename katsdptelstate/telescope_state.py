2e8d5a2dacaf9702967a3b92a58164ef13d088de
Simon Ratcliffe <cari@nanolabs.co.za>
2015-01-22 14:49:45 +0200

    Added getitem syntax and check for collision with base class method names

--- katsdptelstate/telescope_state.py
+++ katsdptelstate/telescope_state.py
@@ -2,15 +2,21 @@ import redis
 import struct
 import time
 import cPickle
+import logging
 
-class TelescopeModel(object):
+logger = logging.getLogger(__name__)
+
+class InvalidKeyError(Exception):
+    pass
+
+class TelescopeState(object):
     def __init__(self, host='localhost', db=0):
         self._r = redis.StrictRedis(db=db)
         self._ps = self._r.pubsub(ignore_subscribe_messages=True)
         self._default_channel = 'tm_info'
         self._ps.subscribe(self._default_channel)
          # subscribe to the telescope model info channel
-
+        
     def _strip(self, str_val):
         if len(str_val) < 8: return None
         ts = struct.unpack('>d',str_val[:8])[0]
@@ -19,30 +25,29 @@ class TelescopeModel(object):
         except cPickle.UnpicklingError:
             ret_val = str_val[8:]
         return (ret_val,ts)
-
+        
     def __getattr__(self, key):
-        if self._r.exists(key):
-            try:
-                return self._r.get(key)
-                 # assume simple string type
-            except redis.ResponseError:
-                try:
-                    return self._strip(self._r.zrange(key,-1,-1)[0])[0]
-                except redis.ResponseError:
-                    raise AttributeError
-
+        val = self._get(key)
+        if val is None: raise AttributeError
+        return val
+        
+    def __getitem__(self,key):
+        val = self._get(key)
+        if val is None: raise KeyError
+        return val
+    
     def send_message(self, data, channel=None):
         """Broadcast a message to all telescope model users."""
         if channel is None: channel = self._default_channel
         return self._r.publish(channel, data)
-
+    
     def get_message(self, channel=None):
         """Get the oldest unread telescope model message."""
         if channel is None: channel = self._default_channel
         msg = self._ps.get_message(channel)
         if msg is not None: msg = msg['data']
         return msg
-
+    
     def list_keys(self, filter='*', show_counts=False):
         """Return a list of keys currently in the model."""
         key_list = []
@@ -57,23 +62,48 @@ class TelescopeModel(object):
         else:
             key_list = self._r.keys(filter)
         return key_list
-
+    
     def delete(self, key):
         """Remove a key, and all values, from the model."""
         return self._r.delete(key)
-
-    def add(self, key, value, ts=None):
+    
+    def add(self, key, value, ts=None, immutable=False):
         """Add a new key / value pair to the model."""
-        if ts is None: ts = time.time()
+        if self.__class__.__dict__.has_key(key):
+            raise InvalidKeyError("The specified key already exists as a class method and thus cannot be used.")
+         # check that we are not going to munge a class method
+        if ts is None and not immutable: ts = time.time()
         existing_type = self._r.type(key)
-        if existing_type != 'none' and existing_type != 'zset':
-            self._r.delete(key)
-        packed_ts = struct.pack('>d',float(ts))
-        return self._r.zadd(key, 0, "{}{}".format(packed_ts,cPickle.dumps(value)))
-
-    def get(self, key, st=None, et=None):
+        if existing_type == 'string':
+            logger.error("Attempt to overwrite immutable key.")
+            return 0
+        if immutable:
+            return self._r.set(key, cPickle.dumps(value))
+        else:
+            packed_ts = struct.pack('>d',float(ts))
+            return self._r.zadd(key, 0, "{}{}".format(packed_ts,cPickle.dumps(value)))
+    
+    def _get(self, key):
+        if self._r.exists(key):
+            try:
+                str_val = self._r.get(key)
+                 # assume simple string type for immutable
+                try:
+                    return cPickle.loads(str_val)
+                except cPickle.UnpicklingError:
+                    return str_val
+            except redis.ResponseError:
+                return self._strip(self._r.zrange(key,-1,-1)[0])[0]
+        return None
+    
+    def get(self, key):
+        val = self._get(key)
+        if val is None: raise KeyError
+        return val
+    
+    def get_range(self, key, st=None, et=None):
         """Get the value specified by the key from the model."""
-        if not self._r.exists(key): return KeyError
+        if not self._r.exists(key): raise KeyError
         if st is None and et is None:
             return self._strip(self._r.zrange(key,-1,-1)[0])
         elif st == 0 or et == 0:
@@ -83,4 +113,4 @@ class TelescopeModel(object):
             packed_et = struct.pack('>d',float(et))
             ret_vals = self._r.zrangebylex(key,"[{}".format(packed_st),"[{}".format(packed_et))
             return [self._strip(str_val) for str_val in ret_vals]
-
+            
