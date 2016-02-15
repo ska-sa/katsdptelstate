13e2675624458fea33526d30c3b6f217af203266
Ludwig Schwardt <ludwig@ska.ac.za>
2016-02-15 23:57:05 +0200

    Make setup.py pass PEP8 (no special formatting!)

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup (
-    name = "katsdptelstate",
-    description = "Karoo Array Telescope - Telescope State Client",
-    author = "Simon Ratcliffe",
-    author_email="sratcliffe@ska.ac.za",
-    url='https://github.com/ska-sa/katsdptelstate',
-    packages = find_packages(),
-    package_data={'': ['conf/*']},
-    include_package_data = True,
-    install_requires = ['redis', 'netifaces', 'numpy'],
-    tests_require = ['mock'],
-    scripts = [],
-    zip_safe = False,
-    setup_requires=['katversion'],
-    use_katversion=True,
-)
+
+setup(name="katsdptelstate",
+      description="Karoo Array Telescope - Telescope State Client",
+      author="Simon Ratcliffe",
+      author_email="sratcliffe@ska.ac.za",
+      url='https://github.com/ska-sa/katsdptelstate',
+      packages=find_packages(),
+      package_data={'': ['conf/*']},
+      include_package_data=True,
+      install_requires=['redis', 'netifaces', 'numpy'],
+      tests_require=['mock'],
+      scripts=[],
+      zip_safe=False,
+      setup_requires=['katversion'],
+      use_katversion=True)
