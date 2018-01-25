cce29e633584d1af83dcc99293856a261febd835
Simon Ratcliffe <simonr@ska.ac.za>
2018-01-25 14:31:07 +0200

    Removed unneeded tests now that fakenewsredis is being used to back tabloid redis

--- katsdptelstate/test/test_rdb_handling.py
+++ katsdptelstate/test/test_rdb_handling.py
@@ -28,21 +28,6 @@ class TestRDBHandling(unittest.TestCase):
             os.remove(self.base_dir)
         except OSError: pass
 
-    def test_basic_operations(self):
-        self.assertEqual(self.tr.keys(), [])
-        self.tr.set('foo','bar')
-        self.assertEqual(self.tr.get('foo'), b'bar')
-        self.tr.flushdb()
-        self.assertEqual(self.tr.get('fooled'), None)
-        self.tr.flushdb()
-
-    def test_zset_operations(self):
-        for x in range(10): 
-            self.tr.zadd('fooz',0,'item_{}'.format(x))
-        self.assertEqual(self.tr.zcard('fooz'), 10)
-        self.assertEqual(self.tr.zrange('fooz',3,3)[0], b'item_3')
-        self.tr.flushdb()
-
     def _enc_ts(self, ts):
         return struct.pack('>d', ts)
 
@@ -51,24 +36,6 @@ class TestRDBHandling(unittest.TestCase):
         self.tr.zadd(key,0,self._enc_ts(ts + 2) + b'third')
         self.tr.zadd(key,0,self._enc_ts(ts + 1) + b'second')
         
-    def test_zset_lex(self):
-        base_ts = int(time.time())
-        self._add_test_vec('foozl', base_ts)
-        sorted_items = self.tr.zrangebylex('foozl','-','+')
-        self.assertEqual(len(sorted_items), 3)
-        self.assertEqual(sorted_items[1][8:], b'second')
-       
-         # test fully open interval
-        sorted_single = self.tr.zrangebylex('foozl',b'(' + self._enc_ts(base_ts + 0.001), b'(' + self._enc_ts(base_ts + 2))
-        self.assertEqual(len(sorted_single), 1)
-        self.assertEqual(sorted_single[0][8:], b'second')
-
-         # reverse half open
-        sorted_rev = self.tr.zrevrangebylex('foozl',b'[' + self._enc_ts(base_ts + 2.001), b'(' + self._enc_ts(base_ts + 0.001))
-        self.assertEqual(len(sorted_rev), 2)
-        self.assertEqual(sorted_rev[0][8:], b'third')
-        self.tr.flushdb()
-
     def test_writer_reader(self):
         base_ts = int(time.time())
         self._add_test_vec('writezl', base_ts)
