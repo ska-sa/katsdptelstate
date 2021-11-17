622f8cc762d7054ad8596950201ad49267452b3e
Bruce Merry <bmerry@sarao.ac.za>
2021-11-17 08:14:07 +0200

    Fix set_indexed so that wait_indexed actually works
    The Lua script for set_indexed was using a hex escape `\x03`, which is
    only supported from Lua 5.2, and the Lua in redis is Lua 5.1.

--- katsdptelstate/lua_scripts/set_indexed.lua
+++ katsdptelstate/lua_scripts/set_indexed.lua
@@ -1,6 +1,6 @@
 if redis.call('HSETNX', KEYS[1], ARGV[1], ARGV[2]) == 1 then
     -- Bundle subkey and value into a single message in a way that allows unpacking
-    local message = "\x03" .. string.len(ARGV[1]) .. "\n" .. ARGV[1] .. ARGV[2]
+    local message = "\003" .. string.len(ARGV[1]) .. "\n" .. ARGV[1] .. ARGV[2]
     redis.call('PUBLISH', 'update/' .. KEYS[1], message)
     return false
 else
